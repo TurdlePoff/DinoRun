dab9447bc9c0677e347c41f766adde8b37fef35f
Vivian <seikyura@gmail.com>
2019-08-28 13:32:47 +1200

    Added main menu script that will start the game. Methods are linked to the buttons.

---
+++ DinoRun/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OpenAchievements()
+    {
+        //put code here
+    }
+
+    public void OpenSettings()
+    {
+        //put code here
+    }
+
+    public void OpenShop()
+    {
+        //put code here
+    }
+
+    public void PlayGame()
+    {
+        //put code here
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ShareToFacebook()
+    {
+        //put code here
+    }
+
+    public void ShareToTwitter()
+    {
+        //put code here
+    }
+}
