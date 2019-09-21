eb5654a11020801a286478d42c011cc16120c4f3
Sally <sallygillbanks@gmail.com>
2019-09-21 15:44:45 +1200

    Twitter push

--- DinoRun/Assets/Scripts/FacebookTwitter.cs
+++ DinoRun/Assets/Scripts/FacebookTwitter.cs
@@ -22,7 +22,7 @@ public class FacebookTwitter : MonoBehaviour
         string twitterAddress = "http://twitter.com/intent/tweet";
         string message = "GET THIS EPIC ENDLESS RUNNER";//text string
         string descriptionParameter = "Dino Run by Dinosaur Forestry Club";
-        string appStoreLink = "https://play.google.com/apps/testing/com.DinosaurForestryClub.SpeedyBoi id = com.growlgamesstudio.pizZapMania";
+        string appStoreLink = "https://play.google.com/apps/testing/com.DinosaurForestryClub.SpeedyBoi"; // Immediatly after i in Boi -> ? id = com.DinosaurForestryClub.SpeedyBoi
 
         Application.OpenURL(twitterAddress + "?text=" +
         WWW.EscapeURL(message + "\n" +
